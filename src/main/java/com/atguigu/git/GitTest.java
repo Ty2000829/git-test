85ac37053fa3fea6a2a5738588b922e110e7c831
tingyu <1736935315@qq.com>
2022-04-22 15:26:56 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author rpstart
+ * @create 2022-04-22 15:22
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello,git!");
+    }
+}
